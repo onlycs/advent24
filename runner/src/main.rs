38cbe455f8a66d10fbcced4c9b47332d9213c917
Angad Tendulkar <angad.tendulkar@gmail.com>
2024-12-01 09:58:04 -0500

    day1 solutions

---
+++ runner/src/main.rs
@@ -0,0 +1,55 @@
+use libadvent::Solution;
+
+macro_rules! runner {
+    ($($day:ident),*) => {
+        let mut iter = 1..;
+
+        println!("Pick a day:");
+        $(
+            println!("{}) {}", iter.next().unwrap(), concat!(stringify!($day), "a"));
+            println!("{}) {}", iter.next().unwrap(), concat!(stringify!($day), "b"));
+        )*
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim().parse::<usize>().unwrap();
+
+        let mut iter = 1..;
+
+        match input {
+            $(
+                i if i == iter.next().unwrap() => {
+                    $day::_Solution::run(
+                        include_str!(
+                            concat!(
+                                env!("CARGO_MANIFEST_DIR"),
+                                "/../dataset/",
+                                stringify!($day),
+                                ".txt"
+                            )
+                        ),
+                        1
+                    )
+                },
+                i if i == iter.next().unwrap() => {
+                    $day::_Solution::run(
+                        include_str!(
+                            concat!(
+                                env!("CARGO_MANIFEST_DIR"),
+                                "/../dataset/",
+                                stringify!($day),
+                                ".txt"
+                            )
+                        ),
+                        2
+                    )
+                },
+            )*
+            _ => println!("Invalid day"),
+        }
+    };
+}
+
+fn main() {
+    runner!(day01);
+}
---
+++ solutions/day01/src/lib.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use libadvent::{NewlineSeperated, Solution, WhiteSeperated};
+
+pub struct _Solution;
+
+impl Solution for _Solution {
+    type Input = NewlineSeperated<WhiteSeperated<i32>>;
+    type Output1 = i32;
+    type Output2 = i32;
+
+    fn day1(input: Vec<Vec<i32>>) -> i32 {
+        let mut side1 = input.iter().map(|v| v[0]).collect_vec();
+        let mut side2 = input.iter().map(|v| v[1]).collect_vec();
+
+        side1.sort();
+        side2.sort();
+
+        let mut sum = 0;
+        for (a, b) in side1.iter().zip(side2.iter()) {
+            let diff = *a - *b;
+            sum += diff.abs();
+        }
+
+        sum
+    }
+
+    fn day2(input: Vec<Vec<i32>>) -> i32 {
+        let side1 = input.iter().map(|v| v[0]).collect_vec();
+        let side2 = input
+            .iter()
+            .map(|v| v[1])
+            .fold(HashMap::new(), |mut acc, v| {
+                acc.entry(v).and_modify(|e| *e += 1).or_insert(1);
+                acc
+            });
+
+        let mut sum = 0;
+        for a in side1.iter() {
+            let appearances = side2.get(a).copied().unwrap_or(0);
+            let similarity = a * appearances;
+            sum += similarity;
+        }
+
+        sum
+    }
+}
