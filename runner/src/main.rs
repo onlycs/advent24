d50efa79559492a99029858aa074975219f46a53
Angad Tendulkar <angad.tendulkar@gmail.com>
2024-12-17 18:00:13 +0530

    runner: fetch

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,5 +1,8 @@
 use libadvent::Parser;
-use std::io::Write;
+use reqwest::StatusCode;
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use std::path::Path;
 use std::time::Duration;
 
 use colored::Colorize;
@@ -15,14 +18,12 @@ macro_rules! runner {
 
     (parse $day:ident) => {
         $day::parser().parse(
-            include_str!(
-                concat!(
-                    env!("CARGO_MANIFEST_DIR"),
-                    "/../dataset/",
-                    stringify!($day),
-                    ".txt"
-                )
-            ).trim()
+            input(concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/../dataset/",
+                stringify!($day),
+                ".txt"
+            )).as_str()
         )
     };
 
@@ -31,12 +32,19 @@ macro_rules! runner {
         println!("│");
         println!("├─ Format: DD[a|b]");
         println!("├─ Example: 01a");
+        println!("├─ Or enter \"fetch\" to fetch all inputs");
         println!("│");
           print!("╰─ ");
 
         let mut input = String::new();
         std::io::stdout().lock().flush().unwrap();
         std::io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "fetch" {
+            println!("\x1b[1A╰─ {}", "Fetching...".yellow());
+            fetch();
+            return;
+        }
 
         let mut iter = (1..).map(|i| (i + 1) / 2);
 
@@ -59,7 +67,7 @@ macro_rules! runner {
 
                     print!("\x1b[1A╰─ ");
                     println!("{} - {}", stringify!($day).blue(), "Level 1".magenta());
-                    println!("\t{}:\t{}", "output".cyan(), output);
+                    println!("\toutput:\t{}", output.to_string().cyan());
                 },
                 i if i.trim() == format!("{:02}b", iter.next().unwrap()) => {
                     let timer = ::std::time::Instant::now();
@@ -97,6 +105,107 @@ fn colorize(time: Duration, a: u64, b: u64) -> String {
     } else {
         format!("{}", disp.red())
     }
+}
+
+fn input(file: &'static str) -> String {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .open(file)
+        .expect("Could not open file");
+
+    let mut input = String::new();
+
+    file.read_to_string(&mut input)
+        .expect("Could not read file");
+
+    input.trim().to_string()
+}
+
+fn fetch() {
+    // try to open .token.txt
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(concat!(env!("CARGO_MANIFEST_DIR"), "/../.token.txt"))
+        .expect("Could not open .token.txt");
+
+    // check if file empty
+    let mut token = String::new();
+
+    file.read_to_string(&mut token)
+        .expect("Could not read .token.txt");
+
+    if token.is_empty() {
+        println!("\x1b[1A├─ Enter your token");
+        print!("╰─ ");
+
+        std::io::stdout().lock().flush().unwrap();
+        std::io::stdin().read_line(&mut token).unwrap();
+
+        file.write_all(token.as_bytes())
+            .expect("Could not write to .token.txt");
+
+        println!(
+            "\x1b[2A╰─ {}     \n{}  \n\x1b[2A",
+            "Fetching...".yellow(),
+            " ".repeat(token.len())
+        );
+    }
+
+    let token = token.trim();
+
+    for day in 1..=25 {
+        // if file exists, skip
+        if Path::new(&format!(
+            "{}/../dataset/day{:02}.txt",
+            env!("CARGO_MANIFEST_DIR"),
+            day
+        ))
+        .exists()
+        {
+            continue;
+        }
+
+        // attempt to fetch the token using reqwest
+        let url = format!("https://adventofcode.com/2024/day/{}/input", day);
+        let cookie = format!("session={}", token);
+
+        let client = reqwest::blocking::Client::new();
+
+        let response = client
+            .get(&url)
+            .header("cookie", cookie)
+            .send()
+            .expect("Failed to fetch input");
+
+        match response.status() {
+            StatusCode::NOT_FOUND => break,
+            StatusCode::INTERNAL_SERVER_ERROR => {
+                println!("\x1b[2A╰─ {}", "Invalid Token".red());
+                break;
+            }
+            _ => {}
+        }
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(format!(
+                "{}/../dataset/day{:02}.txt",
+                env!("CARGO_MANIFEST_DIR"),
+                day
+            ))
+            .expect("Could not open file");
+
+        file.write_all(&response.bytes().expect("Failed to read response"))
+            .unwrap();
+
+        file.flush().unwrap();
+    }
+
+    println!("\x1b[1A╰─ {}", "Fetched    ".green());
 }
 
 fn main() {
